8ac33856529abc5f0227c87ffa3fd2c48c39beb9
hsejsx <hsejsx@gmail.com>
2022-07-07 19:46:37 +0900

    JS : canvas 반응형 설정

--- app.js
+++ app.js
@@ -1,28 +1,66 @@
 'use strict';
-const canvas = document.querySelector('#canvas');
-const canvasSize = canvas.getBoundingClientRect();
-const ctx = canvas.getContext('2d');
+let canvas;
+let canvasSize;
+let ctx;
 const claerBtn = document.querySelector('.clear-btn');
 const colorBtns = document.querySelector('.color-btns');
 const brush = document.querySelector('#brush');
 let mode = false;
 let size = brush.value;
 
-canvas.addEventListener('mousedown', (e) => {
-    mode = true;
+setScreen();
+
+window.addEventListener('resize', () => {
+    setScreen();
 })
 
-canvas.addEventListener('mouseup', (e) => {
-    mode = false;
-})
-
-canvas.addEventListener('mousemove', (e) => {
-    if (mode) {
-        const x = e.clientX - canvasSize.left;
-        const y = e.clientY - canvasSize.top;
-        painting(x, y);
+function setScreen() {
+    const intViewportWidth = window.innerWidth;
+    console.log(intViewportWidth);
+    if (intViewportWidth <= 575) {
+        setCanvas('#canvas-sm');
+    } else if (intViewportWidth >= 576 && intViewportWidth <= 767) {
+        setCanvas('#canvas-md');
+    } else if (intViewportWidth >= 768) {
+        setCanvas('#canvas-lg');
     }
-})
+}
+
+function setCanvas(size) {
+    console.log(size);
+    canvas = document.querySelector(size);
+    canvasSize = canvas.getBoundingClientRect();
+    ctx = canvas.getContext('2d');
+    canvas.addEventListener('mousedown', (e) => {
+        mode = true;
+    })
+    canvas.addEventListener('touchstart', (e) => {
+        mode = true;
+    })
+
+    canvas.addEventListener('mouseup', (e) => {
+        mode = false;
+    })
+    canvas.addEventListener('touchend', (e) => {
+        mode = false;
+    })
+
+    canvas.addEventListener('mousemove', (e) => {
+        if (mode) {
+            const x = e.clientX - canvasSize.left;
+            const y = e.clientY - canvasSize.top;
+            painting(x, y);
+        }
+    })
+    canvas.addEventListener('touchmove', (e) => {
+        if (mode) {
+            const touch = e.touches[0];
+            const x = touch.clientX - canvasSize.left;
+            const y = touch.clientY - canvasSize.top;
+            painting(x, y);
+        }
+    })
+}
 
 function painting(x, y) {
     ctx.fillRect(x, y, size, size);
